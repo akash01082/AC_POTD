1027dacc396362c6c6c411adb4575469c5811fa9
akash01082 <akashkundu0530@gmail.com>
2026-01-16 14:23:59 +0530

    upload task1 code in cpp

--- day_1/task1.cpp
+++ day_1/task1.cpp
@@ -1,5 +1,12 @@
-#include<iostream>
-using namespace std;
-int main(){
-    return 0;
-}
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int num=0,n;
+        for(int i=0;i<nums.size();i++){
+            int n=count(nums.begin() + i + 1, nums.end(), nums[i]);
+            num+=n;
+        }
+        return num;
+
+    }
+};
