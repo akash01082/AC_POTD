34590ff0fba768c7c9b3652a82456db1b16da8f0
akash01082 <akashkundu0530@gmail.com>
2026-01-17 21:31:08 +0530

    add task2 code

---
+++ day_1/task2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int k,n,w,totalCost=0;
+    cin>>k>>n>>w;
+    for(int i=1;i<=w;i++){
+        totalCost+=(k*i);
+    }
+    int borrow=totalCost-n;
+    if(borrow<=0) cout<<0<<endl;
+    else cout<<borrow<<endl;
+    return 0;
+}
