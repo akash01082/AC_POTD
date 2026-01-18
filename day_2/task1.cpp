a310f8c8e7d623a3ade0bdd2b6138a3c874d6975
akash01082 <akashkundu0530@gmail.com>
2026-01-18 21:04:37 +0530

    add day2 task1 code & create day3 folder

---
+++ day_2/task1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int count=0;
+        int n;
+        cin>>n;
+        if(n%2==0){
+            int chckn=n/2;
+            while(chckn>=0){
+                count++;
+                chckn=chckn-2;
+            }
+            cout<<count<<endl;
+        }
+        else 
+            cout<<count<<endl;
+    }
+}
