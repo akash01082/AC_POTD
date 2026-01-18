dcb11e06ee7f3affb2c2364258241a6d45a913a8
akash01082 <akashkundu0530@gmail.com>
2026-01-18 10:13:51 +0530

    adding my day2 task2 code

---
+++ day_2/task2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int calPoints(vector<string>& operations){
+    vector<int> result;
+    for(int i=0;i<operations.size();i++){
+        string str =operations[i];
+        if(str=="C"){
+            result.pop_back();
+        }
+        else if (str=="D"){
+             int Double=2*(result[result.size()-1]);
+             result.push_back(Double);
+        }
+        else if(str=="+"){
+            int add=result[result.size()-1] + result[result.size()-2];
+            result.push_back(add);
+        }else{
+            int num = stoi(operations[i]);
+            result.push_back(num);
+        }
+    }
+    int sum=0;
+    for(int it: result){
+        sum+=it;
+    }
+    return sum;
+
+}
+int main(){
+    vector<string> ops = {"5","-2","4","C","D","9","+","+"};
+    cout<<calPoints(ops)<<endl;
+}
