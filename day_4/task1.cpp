35e1bce2bfe3de6dfe7bd401248e60341f05602f
akash01082 <akashkundu0530@gmail.com>
2026-01-19 19:05:58 +0530

    day4 task1 added

---
+++ day_4/task1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findGCD(vector<int>& nums) {
+        int min=nums[0], max=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>max)
+              max=nums[i];
+            if(nums[i]<min)
+              min=nums[i];
+        }
+        return __gcd(min,max);
+    }
+
+    int main(){
+        vector<int> nums ={2,5,6,9,10};
+        cout<<findGCD(nums)<<endl;
+    }
