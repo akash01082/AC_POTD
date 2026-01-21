3f1b9964efc5edf7b6964de8fab7f5a0f96a7bee
akash01082 <akashkundu0530@gmail.com>
2026-01-21 22:24:00 +0530

    add day5 task1 code

---
+++ day_5/task1.cpp
@@ -0,0 +1,19 @@
+//Codeforce 282/A : Bit++
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int x=0;
+    cin>>n;
+    while(n--){
+        string str;
+        cin>>str;
+        if(str=="X++" || str=="++X")
+            x++;
+        else
+            x--;
+    }
+    cout<<x<<endl;
+
+}
