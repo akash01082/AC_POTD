9a6f88c774e1639423dadfa1d4d341855758eb54
akash01082 <akashkundu0530@gmail.com>
2026-01-23 10:41:55 +0530

    adding day14 task1

---
+++ day_14/task1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> ans(m,vector<int>(n));
+        if(original.size()!=m*n)
+          return {};
+        int idx=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[i][j]=original[idx];
+                idx++;
+            }
+        }
+        return ans;
+    }
+
+int main(){
+    
+}
