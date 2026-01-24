9317e8c1e56d099f40f843e45e8f0995a91a9957
akash01082 <akashkundu0530@gmail.com>
2026-01-24 20:29:05 +0530

    adding day6 task2 code

---
+++ day_6/task2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        bool check=false;
+        string s;
+        cin>>s;
+        for(int i=1;i<s.length()/2;i++){
+            if(s[i]!=s[i-1]){
+                check=true;
+            }
+        }
+        if(check) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
