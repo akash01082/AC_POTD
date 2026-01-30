90392dcb44b2f2c2df7e5e6dd58c0c2f5f6dcb6b
akash01082 <akashkundu0530@gmail.com>
2026-01-30 09:49:17 +0530

    adding day22 task1 code

---
+++ day_22/task1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> bestBuy(n);
+        bestBuy[0]=prices[0];
+        int currProfit,maxProfit=0;
+        for(int i=1;i<n;i++){
+            bestBuy[i]=min(prices[i-1],bestBuy[i-1]);
+            currProfit=prices[i]-bestBuy[i];
+            maxProfit=max(currProfit,maxProfit);
+        }
+        return maxProfit;
+}
+
+int main(){
+    
+}
