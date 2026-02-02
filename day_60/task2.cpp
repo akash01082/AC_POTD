9203c7ad95047de3e8910c910c9258025ce7ce24
akash01082 <akashkundu0530@gmail.com>
2026-02-02 18:13:44 +0530

    adding day60 task2 code

---
+++ day_60/task2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findSum(int n) {
+        // code here
+    if(n<1)
+        return 0;
+    if(n==1){
+        return 1;
+    }
+    int sum=n+findSum(n-1);
+    return sum;
+}
+
+int main(){
+    
+}
