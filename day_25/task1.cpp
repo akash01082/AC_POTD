8f793e75acbb991b5fe094eb7caea60e05ec33fe
akash01082 <akashkundu0530@gmail.com>
2026-02-11 07:46:21 +0530

    adding day25 task1 code

---
+++ day_25/task1.cpp
@@ -0,0 +1,19 @@
+// Leet code 20 : Valid Parentheses
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);
+        }else{
+            if(st.empty()) return false;
+            if( (s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{'))
+                st.pop();
+            else return false;
+        }
+    }
+    return st.empty();
+}
