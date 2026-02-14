4aa970c8b2107c2dce7a03c12b200cacaede16f1
akash01082 <akashkundu0530@gmail.com>
2026-02-14 13:54:13 +0530

    adding day25 task2 code

---
+++ day_25/task2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    string reverse(const string& S) {
+        // code here
+        stack<int> stk;
+        for(int i=0;i<S.size();i++){
+            char ch=S[i];
+            stk.push(ch);
+        }
+        string res;
+        while(!stk.empty()){
+            char ch=stk.top();
+            stk.pop();
+            res+=ch;
+        }
+        return res;
+    }
