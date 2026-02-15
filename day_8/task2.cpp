ba226ee9a5b3a62f1c8ea22ce5787ddc38b816f5
akash01082 <akashkundu0530@gmail.com>
2026-02-15 11:29:42 +0530

    adding day8 task2 code

--- day_8/task2.cpp
+++ day_8/task2.cpp
@@ -6,13 +6,36 @@ int main(){
     cin>>t;
     while (t--)
     {
-        int n;
+        long long n;
         cin>>n;
-        long long arr[n];
+        long long sum=0;
+        vector<long long> nums;
         for(int i=0;i<n;i++){
-            cin>>arr[i];
+            long long number;
+            cin>>number;
+            nums.push_back(number);
+            if(number<0){
+                number*=(-1);
+            }
+            sum+=number;
         }
-        
+        long long count=0;
+        bool check=false;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                check=true;
+            }else if(nums[i]>0){
+                if(check)   count++;
+                check=false;
+            }else{
+                if(check) check=true;
+            }
+            if(i==n-1 && check){
+                if(nums[i]==0 && nums[i-1]>0) continue;
+                count++;
+            }
+        }
+        cout<<sum<<" "<<count<<endl;
     }
     
 }
