15bb76ee81445a481ea2270a05b8c41431dc978a
akash01082 <akashkundu0530@gmail.com>
2026-02-19 17:30:19 +0530

    adding day24 task1 code

---
+++ day_24/task1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStack {
+  public:
+    int top;
+    int size;
+    int* arr;
+    myStack(int n) {
+        // Define Data Structures
+        size=n;
+        top=-1;
+        arr=new int[n];
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        return top==-1;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        return top==size-1;
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        if(isFull()) return;
+        top++;
+        arr[top]=x;
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        if(isEmpty()) return ;
+        top--;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if(isEmpty()) return -1;
+        return arr[top];
+    }
+};
